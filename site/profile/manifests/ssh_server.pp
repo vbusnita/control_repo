866830f0a83dd9486c830d1a5e86b0f764d8d07c
Victor Alexandru Busnita <victorbusnita@me.com>
2023-03-26 22:53:16 -0400

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
                 ensure => present,
                 user   => 'root',
                 type   => 'ssh-rsa',
-                key    => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDEDwV0Mn6h/7bb155hA6AOPCDct8xDl5BdEINjbdv8IOaFyd4lrQJjxp/Ma4QEYLuXHmwlvjhej8w3lepImz7wr+syV6yMRWMu8yImR7Nlp1YFOfRqknyplg1ETDrWuyfhRtSkX2BoXcFeEmKU/7nYkHLBsjUWT9PZ1nD3PtTD4DDo8rUdibp+CMH1XZ5+c02XBoJPdA4+zE2mGfqsiVDdRGF2/BAmJiRCxiY2GAg4mw5dIxbUWS0766ulUG5Bcct22JeYXKMjwNCmYGCehDWhc3k85XnYOdW3mxAlgmfTBM8UJoYmYH6HbSOnFvLMO7+SbHTGMhAj7vTiPbmK2Knj root@master.puppet.vm',
+                key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDEDwV0Mn6h/7bb155hA6AOPCDct8xDl5BdEINjbdv8IOaFyd4lrQJjxp/Ma4QEYLuXHmwlvjhej8w3lepImz7wr+syV6yMRWMu8yImR7Nlp1YFOfRqknyplg1ETDrWuyfhRtSkX2BoXcFeEmKU/7nYkHLBsjUWT9PZ1nD3PtTD4DDo8rUdibp+CMH1XZ5+c02XBoJPdA4+zE2mGfqsiVDdRGF2/BAmJiRCxiY2GAg4mw5dIxbUWS0766ulUG5Bcct22JeYXKMjwNCmYGCehDWhc3k85XnYOdW3mxAlgmfTBM8UJoYmYH6HbSOnFvLMO7+SbHTGMhAj7vTiPbmK2Knj',
       }
 }
